3599d8d829591634a4d001b8e3cc0f8258c2127d
Daniel Pan <danielpan357@gmail.com>
2025-09-18 19:51:21 -0400

    added name & github

--- src/app/Labs/page.tsx
+++ src/app/Labs/page.tsx
@@ -1,22 +1,29 @@
 import Link from "next/link";
 export default function Labs() {
- return (
-   <div id="wd-labs">
-     <h1>Labs</h1>
-     <ul>
-       <li>
-         <Link href="/Labs/Lab1" id="wd-lab1-link">
-           Lab 1: HTML Examples </Link>
-       </li>
-       <li>
-         <Link href="/Labs/Lab2" id="wd-lab2-link">
-           Lab 2: CSS Basics </Link>
-       </li>
-       <li>
-         <Link href="/Labs/Lab3" id="wd-lab3-link">
-           Lab 3: JavaScript Fundamentals </Link>
-       </li>
-     </ul>
-   </div>
-);}
-
+  return (
+    <div id="wd-labs">
+      <h1>Labs</h1>
+      <h2>Daniel Pan</h2>
+      <a href="https://github.com/DaanielPan/kambaz-next-js" id="wd-github">
+        My GitHub Repository
+      </a>
+      <ul>
+        <li>
+          <Link href="/Labs/Lab1" id="wd-lab1-link">
+            Lab 1: HTML Examples
+          </Link>
+        </li>
+        <li>
+          <Link href="/Labs/Lab2" id="wd-lab2-link">
+            Lab 2: CSS Basics
+          </Link>
+        </li>
+        <li>
+          <Link href="/Labs/Lab3" id="wd-lab3-link">
+            Lab 3: JavaScript Fundamentals
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+}

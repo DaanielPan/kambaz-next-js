3795aaae3659ee37e905c115bf06591a660e08bd
Daniel Pan <danielpan357@gmail.com>
2025-10-17 15:13:56 -0400

    fixed please work

--- src/app/(Kambaz)/Dashboard/page.tsx
+++ src/app/(Kambaz)/Dashboard/page.tsx
@@ -1,29 +1,34 @@
+"use client";
 import Link from "next/link";
 import { Row, Col, Card, Button } from "react-bootstrap";
 import * as db from "../Database";
 
 export default function Dashboard() {
   const courses = db.courses;
+
   return (
     <div id="wd-dashboard">
-      <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
+      <h1 id="wd-dashboard-title">Dashboard</h1>
+      <hr />
       <h2 id="wd-dashboard-published">
         Published Courses ({courses.length})
-      </h2>{" "}
+      </h2>
       <hr />
+
       <div id="wd-dashboard-courses">
         <Row xs={1} md={5} className="g-4">
-          {courses.map((course) => (
+          {courses.map((course: any) => (
             <Col key={course._id} style={{ width: "300px" }}>
-              <Card>
-                <Link
-                  href={`/Courses/${course._id}/Home`}
-                  className="text-decoration-none text-dark"
-                >
+              <Link
+                href={`/Courses/${course._id}/Home`}
+                className="text-decoration-none text-dark"
+              >
+                <Card>
                   <Card.Img
                     variant="top"
-                    src={`/images/${course.image}`}
+                    src={`/images/${course.image || "default.jpg"}`}
                     height={150}
+                    alt={`Image for ${course.name}`}
                   />
                   <Card.Body>
                     <Card.Title className="text-nowrap overflow-hidden">
@@ -37,8 +42,8 @@ export default function Dashboard() {
                     </Card.Text>
                     <Button variant="primary">Go</Button>
                   </Card.Body>
-                </Link>
-              </Card>
+                </Card>
+              </Link>
             </Col>
           ))}
         </Row>

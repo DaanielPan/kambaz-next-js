38dafb9326448b950d1f317fdf621d5b50911ae6
Daniel Pan <danielpan357@gmail.com>
2025-10-30 17:28:41 -0400

    fix 6

--- src/app/Labs/Lab4/ReduxExamples/HelloRedux/index.tsx
+++ src/app/Labs/Lab4/ReduxExamples/HelloRedux/index.tsx
@@ -2,7 +2,7 @@
 export const dynamic = "force-dynamic";
 import { useSelector, useDispatch } from "react-redux";
 export default function HelloRedux() {
-  const { message } = useSelector((state: any) => state.helloReducer);
+  const message = useSelector((state: any) => state?.hello?.message ?? "");
   return (
     <div id="wd-hello-redux">
       <h3>Hello Redux</h3>

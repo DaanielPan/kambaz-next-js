07d1b5046903c368d7b0c5cf9420814e3f11af35
Daniel Pan <danielpan357@gmail.com>
2025-10-30 17:49:54 -0400

    pls work this time

--- src/app/Labs/Lab4/page.tsx
+++ src/app/Labs/Lab4/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 export const dynamic = "force-dynamic";
-export const revalidate = 0;
+export const fetchCache = "force-no-store"; 
+export const revalidate = false;
 import ClickEvent from "./ClickEvent";
 import PassingDataOnEvent from "./PassingDataOnEvent";
 import PassingFunctions from "./PassingFunctions";

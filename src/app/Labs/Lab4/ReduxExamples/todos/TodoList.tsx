d139c2d5e880dfdaffc9b1e4eb3007313915c143
Daniel Pan <danielpan357@gmail.com>
2025-10-30 17:34:55 -0400

    fix 9

--- src/app/Labs/Lab4/ReduxExamples/todos/TodoList.tsx
+++ src/app/Labs/Lab4/ReduxExamples/todos/TodoList.tsx
@@ -1,4 +1,6 @@
 "use client";
+export const dynamic = "force-dynamic";
+
 import React from "react";
 import { ListGroup } from "react-bootstrap";
 import TodoForm from "./TodoForm";
@@ -6,7 +8,7 @@ import TodoItem from "./TodoItem";
 import { useSelector } from "react-redux";
 
 export default function TodoList() {
-  const { todos } = useSelector((state: any) => state.todosReducer);
+  const todos = useSelector((state: any) => state?.todosReducer?.todos ?? []);
 
   return (
     <div id="wd-todo-list-redux">

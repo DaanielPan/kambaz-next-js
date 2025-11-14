bad6180c567ab0401df64ef601c22ccc2e3a1920
Daniel Pan <danielpan357@gmail.com>
2025-11-14 17:53:26 -0500

    added use client

--- src/app/Labs/Lab5/page.tsx
+++ src/app/Labs/Lab5/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import EnvironmentVariables from "./EnvironmentVariables";
 import PathParameters from "./PathParameters";
 import QueryParameters from "./QueryParameters";
@@ -14,26 +15,27 @@ import WorkingWithObjectsAsynchronously from "./WorkingWithObjectsAsynchronously
 
 const HTTP_SERVER = process.env.NEXT_PUBLIC_HTTP_SERVER;
 export default function Lab5() {
-  return (
-    <div id="wd-lab5">
-      <h2>Lab 5</h2>
-      <div className="list-group">
-        <a href={`${HTTP_SERVER}/lab5/welcome`} className="list-group-item">
-          Welcome
-        </a>
-      </div><hr />
-      <EnvironmentVariables />
-      <PathParameters />
-      <QueryParameters />
-      <WorkingWithObjects />
-      <WorkingWithArrays />
-      <HttpClientOnLoad />
-      <WorkingWithArraysAsynchronously />
-      <WorkingWithObjectsAsynchronously />
+    return (
+        <div id="wd-lab5">
+            <h2>Lab 5</h2>
+            <div className="list-group">
+                <a href={`${HTTP_SERVER}/lab5/welcome`} className="list-group-item">
+                    Welcome
+                </a>
+            </div><hr />
+            <EnvironmentVariables />
+            <PathParameters />
+            <QueryParameters />
+            <WorkingWithObjects />
+            <WorkingWithArrays />
+            <HttpClientOnLoad />
+            <WorkingWithArraysAsynchronously />
+            <WorkingWithObjectsAsynchronously />
 
 
 
 
-    </div>
-);}
+        </div>
+    );
+}
 
